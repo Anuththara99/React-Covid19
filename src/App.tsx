e42686ddb9a3e42f2b34e330bd2b7c6b846c17f5
anuththara <rajananda046@gmail.com>
2021-09-16 11:44:52 +0530

    Create main two tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-function App() {
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      
+    >
+      {value === index && (
+        <Box p={2}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   );
 }
 
-export default App;
+function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+ 
+
+  return (
+    <div className="App">
+      <div style={{height:400, backgroundImage: 'url(img/covidbg.png)',backgroundRepeat:"no-repeat",backgroundPosition:"center"}}>
+
+      </div>
+      <div >
+        <Tabs variant='fullWidth' value={value} onChange={handleChange} centered >
+          <Tab label="Global Coronavirus Information"  />
+          <Tab label="User Data"  />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+        Global Coronavirus Information
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+        User Data
+        </TabPanel>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App
